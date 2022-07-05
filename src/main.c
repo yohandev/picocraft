7d2218e853211037ae7ba80a4422a2400324b136
yohandev <yohang@mit.edu>
2022-07-05 15:58:01 -0700

    Added `uf2` flashing via `cargo-flash`

--- src/main.c
+++ src/main.c
@@ -34,9 +34,9 @@ int main() {
         st7789_draw(&lcd, frame);
 
         gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(100);
+        sleep_ms(500);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(100);
+        sleep_ms(500);
     }
 
     st7789_drop(&lcd);
