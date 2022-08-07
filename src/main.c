58eb10e078fd9024c2cb096c94865338bb9cca90
yohandev <yohang@mit.edu>
2022-08-06 17:13:48 -0700

    Experimenting with C macro magic

--- src/main.c
+++ src/main.c
@@ -1,30 +1,40 @@
 #include "hardware/gpio.h"
 
-#include "st7789.h"
-#include "draw.h"
+// #include "st7789.h"
+// #include "draw.h"
 #include "test.bmp.h"
 
-ST7789 lcd = {
-    .width=WIDTH, .height=HEIGHT,
-    .sck=18, .mosi=19, .dc=3, .rst=2,
-    .spi=spi0, .baudrate=125000000,
-};
-rgb565 frame[WIDTH*HEIGHT] = {0};
+// ST7789 lcd = {
+//     .width=WIDTH, .height=HEIGHT,
+//     .sck=18, .mosi=19, .dc=3, .rst=2,
+//     .spi=spi0, .baudrate=125000000,
+// };
+// rgb565 frame[WIDTH*HEIGHT] = {0};
 
 int main() {
-    st7789_init(&lcd);
+    // st7789_init(&lcd);
 
-    // Draw
-    // frame_fill((rgb565){.r=0, .g=5, .b=0});
-    memcpy(frame, TEST_BMP, WIDTH*HEIGHT*sizeof(rgb565));
-    while (true) {
-        frame_draw_line(
-            (vec2){.x=fixed_from_i32(10), .y=fixed_from_i32(10)},
-            (vec2){.x=fixed_from_i32(200), .y=fixed_from_i32(100)},
-            (rgb565){.r=0, .g=63, .b=31}
-        );
-        st7789_draw(&lcd, frame);
-    }
+    // // Draw
+    // // frame_fill((rgb565){.r=0, .g=5, .b=0});
+    // memcpy(frame, TEST_BMP, WIDTH*HEIGHT*sizeof(rgb565));
 
-    st7789_drop(&lcd);
+    const fixed PI = fixed(3.1415);
+    const fixed TAU = add(PI, PI);
+
+    vec2 foo = {PI, {0}};
+    vec2 bar = {PI, PI};
+
+    vec2 baz = add(foo, bar);
+
+
+    // while (true) {
+    //     frame_draw_line(
+    //         (vec2){.x=fixed(3.1415), .y=fixed(1)},
+    //         (vec2){.x=fixed_from_i32(200), .y=fixed_from_i32(100)},
+    //         (rgb565){.r=0, .g=63, .b=31}
+    //     );
+    //     st7789_draw(&lcd, frame);
+    // }
+
+    // st7789_drop(&lcd);
 }
--- src/num.h
+++ src/num.h
@@ -23,10 +23,31 @@ typedef uint64_t u64;
 typedef int64_t i64;
 // Floating point
 typedef float f32;
+typedef double f64;
 
 // 24.8 fixed point, ARM implementation based on the paper:
 // https://developer.arm.com/documentation/dai0033/a/
-typedef i32 fixed;
+typedef struct {
+    i32 v[1];
+} fixed;
+
+// 2D vector of `fixed`
+typedef union {
+    struct {
+        fixed x;
+        fixed y;
+    };
+    i32 v[2];
+} vec2;
+// Fixed 3D vector
+typedef union {
+    struct {
+        fixed x;
+        fixed y;
+        fixed z;
+    };
+    i32 v[3];
+} vec3;
 
 // RGB565
 typedef union {
@@ -49,44 +70,119 @@ typedef union {
     u32 rgba;
 } rgba32;
 
-// Fixed 2D vector
-typedef struct {
-    fixed x;
-    fixed y;
-} vec2;
-// Fixed 3D vector
-typedef struct {
-    fixed x;
-    fixed y;
-    fixed z;
-} vec3;
-
-// Integer 2D vector
-typedef struct {
-    i32 x;
-    i32 y;
-} vec2i;
-// Integer 3D vector
-typedef struct {
-    i32 x;
-    i32 y;
-    i32 z;
-} vec3i;
+// // Integer 2D vector
+// typedef union {
+//     struct {
+//         i32 x;
+//         i32 y;
+//     };
+//     i32 vec[2];
+// } vec2i;
+// // Integer 3D vector
+// typedef union {
+//     struct {
+//         i32 x;
+//         i32 y;
+//         i32 z;
+//     };
+//     i32 vec[3];
+// } vec3i;
 
 #define FIXED_FRAC_BITS 8
 
-inline fixed fadd(fixed lhs, fixed rhs) { return (lhs + rhs); }
-inline fixed fsub(fixed lhs, fixed rhs) { return (lhs + rhs); }
-inline fixed fmul(fixed lhs, fixed rhs) { return (lhs * rhs) >> FIXED_FRAC_BITS; }
-inline fixed fdiv(fixed lhs, fixed rhs) { return (lhs << FIXED_FRAC_BITS) / rhs; }
+// Reduces a type to a primitive by reinterpreting
+#define num_inner(z) _Generic((z),                  \
+    fixed: (((union {                               \
+        __typeof__(z) from;                         \
+        i32 into;                                   \
+    }){.from=(z)}).into),                           \
+    default: (z)                                    \
+)
+// Casts between numeric types
+#define num_cast(T, y) _Generic((y),                \
+    fixed: ((T)num_inner((y)) >> FIXED_FRAC_BITS),  \
+    default: ((T)num_inner((y)))                    \
+)
 
-inline fixed fixed_from_i32(i32 i) { return (fixed){(i << FIXED_FRAC_BITS)}; }
-inline fixed fixed_from_f32(f32 f) { return (fixed){(f * (1 << FIXED_FRAC_BITS))}; }
-inline i32 fixed_into_i32(fixed self) { return self >> FIXED_FRAC_BITS; }
-inline f32 fixed_into_f32(fixed self) { return (f32)self / (1 << FIXED_FRAC_BITS); }
+// 8-bit
+#define u8(x) num_cast(u8, (x))
+#define i8(x) num_cast(i8, (x))
+// 16-bit
+#define u16(x) num_cast(u16, (x))
+#define i16(x) num_cast(i16, (x))
+// 32-bit
+#define u32(x) num_cast(u32, (x))
+#define i32(x) num_cast(i32, (x))
+// 64-bit
+#define u64(x) num_cast(u64, (x))
+#define i64(x) num_cast(i64, (x))
+// fixed
+#define fixed(x) _Generic((x),                      \
+    int: ((fixed){((i32)(x) << FIXED_FRAC_BITS)}),  \
+    i32: ((fixed){((i32)(x) << FIXED_FRAC_BITS)}),  \
+    f32: ((fixed){((x) * (1 << FIXED_FRAC_BITS))}), \
+    f64: ((fixed){((x) * (1 << FIXED_FRAC_BITS))})  \
+)
 
-inline fixed fixed_max(fixed a, fixed b) { return a > b ? a : b; }
-inline fixed fixed_min(fixed a, fixed b) { return a < b ? a : b; }
+#define add(a, b) _Generic((a),                     \
+    fixed: ((fixed){(a).v[0] + (b).v[1]}),          \
+    vec2: ((vec2){                                  \
+        (a).v[0] + (b).v[0],                        \
+        (a).v[1] + (b).v[1],                        \
+    }),                                             \
+    vec3: ((vec3){                                  \
+        (a).v[0] + (b).v[0],                        \
+        (a).v[1] + (b).v[1],                        \
+        (a).v[2] + (b).v[2],                        \
+    })                                              \
+)
+// #define add(a, b) ({                                \
+//     __typeof__(a) _a = (a);                         \
+//     __typeof__(a) _b = (b);                         \
+//     _Generic((a),                                   \
+//         fixed: ((fixed){num_inner((a)) + num_inner((b))}),  \
+//         vec2: ((vec2){                              \
+//             num_inner((a).vec[0]) + num_inner((b).vec[0]),                \
+//             num_inner((a).vec[1]) + num_inner((b).vec[1]),                \
+//         }),                                         \
+//         vec2i: ((vec2i){                            \
+//             num_inner((a).vec[0]) + num_inner((b).vec[0]),                \
+//             num_inner((a).vec[1]) + num_inner((b).vec[1]),                \
+//         }),                                         \
+//         vec3: ((vec3){                              \
+//             num_inner((a).vec[0]) + num_inner((b).vec[0]),                \
+//             num_inner((a).vec[1]) + num_inner((b).vec[1]),                \
+//             num_inner((a).vec[2]) + num_inner((b).vec[2]),                \
+//         }),                                         \
+//         vec2: ((vec2){                              \
+//             num_inner((a).vec[0]) + num_inner((b).vec[0]),                \
+//             num_inner((a).vec[1]) + num_inner((b).vec[1]),                \
+//             num_inner((a).vec[2]) + num_inner((b).vec[2]),                \
+//         })                                          \
+//     )                                               \
+// })
+// #define add(a, b) ((a) + (b))
+// #define sub(a, b) ((a) - (b))
+// #define mul(a, b) ({                                \
+//     __typeof__(a) _a = (a);                         \
+//     __typeof__(a) _b = (b);                         \
+//     _Generic((a),                                   \
+//         fixed: ((fixed){((inner((a)) * inner((b))) >> FIXED_FRAC_BITS)}),\
+//         default: (inner((a)) * inner((b)))                            \
+//     );                                              \
+// })
+// inline fixed fadd(fixed lhs, fixed rhs) { return (lhs + rhs); }
+// inline fixed fsub(fixed lhs, fixed rhs) { return (lhs + rhs); }
+// inline fixed fmul(fixed lhs, fixed rhs) { return (lhs * rhs) >> FIXED_FRAC_BITS; }
+// inline fixed fdiv(fixed lhs, fixed rhs) { return (lhs << FIXED_FRAC_BITS) / rhs; }
+
+// inline fixed fixed_from_i32(i32 i) { return (fixed){(i << FIXED_FRAC_BITS)}; }
+// inline fixed fixed_from_f32(f32 f) { return (fixed){(f * (1 << FIXED_FRAC_BITS))}; }
+// inline i32 fixed_into_i32(fixed self) { return self.i >> FIXED_FRAC_BITS; }
+// inline f32 fixed_into_f32(fixed self) { return (f32)self / (1 << FIXED_FRAC_BITS); }
+
+// inline fixed fixed_max(fixed a, fixed b) { return a > b ? a : b; }
+// inline fixed fixed_min(fixed a, fixed b) { return a < b ? a : b; }
 
 inline rgba32 rgb_into(rgb565 self) {
     u8 i = self.rgb & 0x1f;
